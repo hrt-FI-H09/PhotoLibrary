3d159dc32e8ada638c60d20a386b6d298a458be6
hrt-FI-H09 <haruto.univ@gmail.com>
2026-01-21 14:02:20 +0900

    Update P_script.js

--- P_script.js
+++ P_script.js
@@ -1,7 +1,7 @@
 const photoFiles = [
     'IMG (1).jpg',
-    'IMG (2).jpg', 'IMG (3).jpg', 'IMG (4).jpg', 'IMG (5).jpg', 'IMG (6).jpg',
-    'IMG (7).jpg', 'IMG (8).jpg', 'IMG (9).jpg', 'IMG (10).JPG', 'IMG (11).JPG',
+    'IMG (2).jpg', 'IMG (3).jpg', 'IMG (4).JPG', 'IMG (5).JPG', 'IMG (6).JPG',
+    'IMG (7).JPG', 'IMG (8).JPG', 'IMG (9).JPG', 'IMG (10).JPG', 'IMG (11).JPG',
     'IMG (12).JPG', 'IMG (13).JPG', 'IMG (14).JPG', 'IMG (15).JPG', 'IMG (16).JPG',
     'IMG (17).JPG', 'IMG (18).JPG', 'IMG (19).JPG', 'IMG (20).JPG', 'IMG (21).JPG',
     'IMG (22).JPG', 'IMG (23).JPG', 'IMG (24).JPG', 'IMG.JPG', 'IMG_3427.JPG',
